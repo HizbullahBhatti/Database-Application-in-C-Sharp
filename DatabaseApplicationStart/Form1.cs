e8934a24fc02828bf1ddb1a05a4b18ca855fc1f4
HizbullahBhatti <hizbullah.kcsf18@iba-suk.edu.pk>
2023-01-09 19:22:54 +0500

    first commit

---
+++ DatabaseApplicationStart/Form1.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+using System.Configuration;
+
+namespace DatabaseApplicationStart
+{
+    public partial class Form1 : Form
+    {
+
+        public static int id;
+        public static string firstName;
+        public static string lastName;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            getUserData();
+        }
+
+        public void getUserData()
+        {
+            String connectionString = ConfigurationManager.ConnectionStrings["root"].ConnectionString;
+            SqlConnection connection = new SqlConnection(connectionString);
+
+            SqlCommand command = new SqlCommand();
+
+            command.Connection = connection;
+
+            command.CommandText = "select * from Users";
+
+            connection.Open();
+
+            SqlDataReader datareader = command.ExecuteReader();
+
+            while (datareader.Read())
+            {
+
+                id = (int)datareader[0];
+
+                firstName = (String)datareader["FirstName"];
+
+                
+                lastName = (String)datareader["LastName"];
+
+                this.lblInfo.Text += id + "\n";
+                this.lblInfo.Text += firstName + "\n";
+                this.lblInfo.Text += lastName + "\n";
+
+            }
+            datareader.Close();
+            connection.Close();
+        
+        }
+    }
+}
